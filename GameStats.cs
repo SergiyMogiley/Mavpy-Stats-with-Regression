f25a8ef024369e48df162de44a7115fd00bae588
SergiyMogiley <97164321+SergiyMogiley@users.noreply.github.com>
2022-07-09 12:25:48 +0300

    Add files via upload

---
+++ GameStats.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+public class GameStats
+{
+    [LoadColumn(0)]
+    public short Location;
+
+    [LoadColumn(1)]
+    public short TwoShots;
+
+    [LoadColumn(2)]
+    public short ThreeShots;
+
+    [LoadColumn(3)]
+    public short Reboundings;
+
+    [LoadColumn(4)]
+    public short Interceptions;
+
+    [LoadColumn(5)]
+    public short Losses;
+
+    [LoadColumn(6)]
+    public short Fouls;
+
+    [LoadColumn(7)]
+    public float ScoresDif;
+}
+
+public class GameResultPrediction
+{
+    [ColumnName("Score")]
+    public float ScoresDif;
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.ML;
+
+string _trainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "Mavpy_14_15_train.csv");
+string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "Mavpy_14_15_test.csv");
+string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "Model_Mavpy.zip");
+
+MLContext mlContext = new MLContext(seed: 0);
+
+var model = Train(mlContext, _trainDataPath);
+
+ITransformer Train(MLContext mlContext, string dataPath)
+{
+    IDataView dataView = mlContext.Data.LoadFromTextFile<GameStats>(dataPath, hasHeader: true, separatorChar: ',');
+
+    var pipeline = mlContext.Transforms.CopyColumns(outputColumnName: "Label", inputColumnName: "ScoresDif")
+
+        .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "LocationEncoded", inputColumnName: "Location"))
+        .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "TwoShotsEncoded", inputColumnName: "TwoShots"))
+        .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "ThreeShotsEncoded", inputColumnName: "ThreeShots"))
+        .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "ReboundingsEncoded", inputColumnName: "Reboundings"))
+        .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "InterceptionsEncoded", inputColumnName: "Interceptions"))
+        .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "LossesEncoded", inputColumnName: "Losses"))
+        .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "FoulsEncoded", inputColumnName: "Fouls"))
+
+        .Append(mlContext.Transforms.Concatenate("Features", "LocationEncoded", "TwoShotsEncoded", "ThreeShotsEncoded",
+                                                 "ReboundingsEncoded", "InterceptionsEncoded", "LossesEncoded", "FoulsEncoded"))
+
+    .Append(mlContext.Regression.Trainers.FastTree());
+
+    var model = pipeline.Fit(dataView);
+
+    mlContext.Model.Save(model, dataView.Schema, _modelPath);
+
+    return model;
+}
+
+Evaluate(mlContext, model);
+
+void Evaluate(MLContext mlContext, ITransformer model)
+{
+    IDataView dataView = mlContext.Data.LoadFromTextFile<GameStats>(_testDataPath, hasHeader: true, separatorChar: ',');
+
+    var predictions = model.Transform(dataView);
+
+    var metrics = mlContext.Regression.Evaluate(predictions, "Label", "Score");
+
+    Console.WriteLine();
+    Console.WriteLine($"*************************************************");
+    Console.WriteLine($"*       Model quality metrics evaluation         ");
+    Console.WriteLine($"*------------------------------------------------");
+
+    Console.WriteLine($"*       RSquared Score:      {metrics.RSquared:0.##}");
+
+    Console.WriteLine($"*       Root Mean Squared Error:      {metrics.RootMeanSquaredError:#.##}");
+}
+
+TestSinglePrediction(mlContext, model);
+
+void TestSinglePrediction(MLContext mlContext, ITransformer model)
+{
+    var predictionFunction = mlContext.Model.CreatePredictionEngine<GameStats, GameResultPrediction>(model);
+
+    var GameStatsSample = new GameStats()
+    {
+        Location = 0,
+        TwoShots = 48,
+        ThreeShots = 27,
+        Reboundings = 46,
+        Interceptions = 5,
+        Losses = 6,
+        Fouls = 15,
+        ScoresDif = 0
+    };
+
+    var prediction = predictionFunction.Predict(GameStatsSample);
+
+    Console.WriteLine($"**********************************************************************");
+    Console.WriteLine($"Predicted scores dif: {prediction.ScoresDif:0.####}, actual scores dif: -9");
+    Console.WriteLine($"**********************************************************************");
+}
